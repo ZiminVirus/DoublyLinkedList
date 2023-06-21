6e959b863b4babc13fb961983ec4e6c34ac7506d
Alexandr <ziminvirus@yandex.ru>
2023-06-21 22:30:53 +0300

    Task_3

---
+++ Main.java
@@ -0,0 +1,100 @@
+public class Main {
+	public static void main(String[] args) {
+		
+
+class Node {
+	int data;
+	Node prev;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.prev = null;
+		this.next = null;
+	}
+}
+
+class DoublyLinkedList {
+	Node head;
+	Node tail;
+
+	public DoublyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public void addNode(int data) {
+		Node newNode = new Node(data);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			newNode.prev = tail;
+			tail = newNode;
+		}
+	}
+
+	public void deleteNode(int data) {
+		Node current = head;
+
+		while (current != null) {
+			if (current.data == data) {
+				if (current == head) {
+					head = current.next;
+					if (head != null) {
+						head.prev = null;
+					}
+				} else if (current == tail) {
+					tail = current.prev;
+					if (tail != null) {
+						tail.next = null;
+					}
+				} else {
+					current.prev.next = current.next;
+					current.next.prev = current.prev;
+				}
+				break;
+			}
+			current = current.next;
+		}
+	}
+
+	public void printList() {
+		Node current = head;
+		while (current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+
+
+	public void reverseList() {
+		if (head == null || head.next == null) {
+			return;
+		}
+
+		Node previous = null;
+		Node current = head;
+		while (current != null) {
+			Node next = current.next;
+			current.next = previous;
+			previous = current;
+			current = next;
+		}
+
+		head = previous;
+	}
+}
+	DoublyLinkedList list = new DoublyLinkedList();
+		list.addNode(1);
+		list.addNode(2);
+		list.addNode(3);
+		list.addNode(4);
+		list.reverseList();
+		list.printList();
+	}
+
+}
